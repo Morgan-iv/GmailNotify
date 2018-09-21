5bfe45098c813e80b3c1b0ce1806c7c0c33733f5
Morgan-iv <ivushkinalex@gmail.com>
2018-09-21 15:29:49 +0300

    tabs to spaces

--- mainbot.py
+++ mainbot.py
@@ -7,37 +7,37 @@ from botauth import number, passwd
 
 
 def main():
-	dbname = "shelvedb"
+    dbname = "shelvedb"
 
-	gmail = GmailApi()
-	newmes = set(gmail.getlist())
+    gmail = GmailApi()
+    newmes = set(gmail.getlist())
 
-	db = shelve.open(dbname)
-	oldmes = db['ids']
-	db.close()
+    db = shelve.open(dbname)
+    oldmes = db['ids']
+    db.close()
 
-	resultstr = ''
-	for mes in newmes - oldmes:
-		tmp = gmail.getmessage(mes)
-		resultstr = resultstr + 'From: {}\nSubject: {}\nSnippet: {}\n\n'.format(tmp[0], tmp[1], tmp[2])
-	#print (resultstr)
+    resultstr = ''
+    for mes in newmes - oldmes:
+        tmp = gmail.getmessage(mes)
+        resultstr = resultstr + 'From: {}\nSubject: {}\nSnippet: {}\n\n'.format(tmp[0], tmp[1], tmp[2])
+    #print (resultstr)
 
-	db = shelve.open(dbname)
-	db['ids'] = newmes
-	db.close()
+    db = shelve.open(dbname)
+    db['ids'] = newmes
+    db.close()
 
-	if not resultstr == '':
-		vk_session = vk_api.VkApi(number, passwd)
-		vk_session.auth()
-		vk = vk_session.get_api()
-		vk.messages.send(random_id=random.randint(0, 65536), peer_id=18269018, message=resultstr)
+    if not resultstr == '':
+        vk_session = vk_api.VkApi(number, passwd)
+        vk_session.auth()
+        vk = vk_session.get_api()
+        vk.messages.send(random_id=random.randint(0, 65536), peer_id=18269018, message=resultstr)
 
 if __name__ == '__main__':
-	main()
-	
-	'''
-	dbname = "shelvedb"
-	db = shelve.open(dbname)
-	db['ids'] = set()
-	db.close()
-	'''
+    main()
+    
+    '''
+    dbname = "shelvedb"
+    db = shelve.open(dbname)
+    db['ids'] = set()
+    db.close()
+    '''
